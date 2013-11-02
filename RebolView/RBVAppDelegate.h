44e3df2d73eed719af3898c1ed3ba2c0f4aa584b
trsoluti <thomas_odell@trsolutions.biz>
2013-11-02 15:39:43 +0800

    Setup initial Xcode project

---
+++ RebolView/RBVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RBVAppDelegate.h
+//  RebolView
+//
+//  Created by TR Solutions Pte Ltd on 2/11/13.
+//  Copyright (c) 2013 TR Solutions Pte Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RBVAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ RebolView/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RebolView
+//
+//  Created by TR Solutions Pte Ltd on 2/11/13.
+//  Copyright (c) 2013 TR Solutions Pte Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+  return NSApplicationMain(argc, argv);
+}
