fca8a02dec19610e8f22073e83a8c76984e6df9d
trsoluti <thomas_odell@trsolutions.biz>
2013-11-02 16:01:14 +0800

    Backed out of original structure

---
+++ RebolLib/RebolLib/RebolLib.h
@@ -0,0 +1,13 @@
+//
+//  RebolLib.h
+//  RebolLib
+//
+//  Created by TR Solutions Pte Ltd on 2/11/13.
+//  Copyright (c) 2013 TR Solutions Pte Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RebolLib : NSObject
+
+@end
---
+++ RebolLib/RebolLib/RebolLib.m
@@ -0,0 +1,13 @@
+//
+//  RebolLib.m
+//  RebolLib
+//
+//  Created by TR Solutions Pte Ltd on 2/11/13.
+//  Copyright (c) 2013 TR Solutions Pte Ltd. All rights reserved.
+//
+
+#import "RebolLib.h"
+
+@implementation RebolLib
+
+@end
